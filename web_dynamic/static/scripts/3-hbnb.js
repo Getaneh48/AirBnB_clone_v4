ddec7b12da7f296671854c34e722a1c5794e40d4
Getaneh48 <getaneh11@gmail.com>
2024-06-03 11:46:03 +0000

    amenity selection added

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+	select_amenities()
 	$.ajax({
 		url: 'http://0.0.0.0:5001/api/v1/status/',
 		type: 'GET',
@@ -16,6 +17,22 @@ $(document).ready(function () {
 		}
 	});
 });
+
+
+function select_amenities() {
+	let selected_amenities = {}
+	$('.popover ul li input[type="checkbox"]').click(function(){
+		const data = $(this).data()
+		if ($(this).is(":checked")) {
+			$
+			selected_amenities[data.id] = data.name
+		} else {
+			delete selected_amenities[data.id]
+		}
+		$('.amenities h4').text(Object.values(selected_amenities).join(', '));
+		console.log(selected_amenities)
+	});
+}
 
 function fetch_places() {
 $.ajax({
